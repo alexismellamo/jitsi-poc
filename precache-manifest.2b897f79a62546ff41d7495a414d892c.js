d34744f2dc9830c8dd897c94d5d5283eabc65163
Alexis Navarro <alexis.navarro@nextiva.com>
2020-05-07 00:43:06 -0500

    Updates

---
+++ precache-manifest.2b897f79a62546ff41d7495a414d892c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58a660a927dadad15ac01f5659bf7bd",
+    "url": "/jitsi-poc/index.html"
+  },
+  {
+    "revision": "6d0f3c29267e68d96ce6",
+    "url": "/jitsi-poc/static/css/main.ff4752a2.chunk.css"
+  },
+  {
+    "revision": "7cc54d635f0c68a5efad",
+    "url": "/jitsi-poc/static/js/2.f60d16ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jitsi-poc/static/js/2.f60d16ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0f3c29267e68d96ce6",
+    "url": "/jitsi-poc/static/js/main.cee4d246.chunk.js"
+  },
+  {
+    "revision": "f196fd0b42d83fd1c724",
+    "url": "/jitsi-poc/static/js/runtime-main.858fc8fa.js"
+  }
+]);
